e1785ea7139b54079c028df5004f14b421daaab5
xpr123 <3424286550@qq.com>
2025-04-30 21:04:24 +0800

    函数

--- math_h/math_h/math.c
+++ math_h/math_h/math.c
@@ -1,1 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
+//abs
+//求整数的绝对值
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int num = -10;
+//	printf("%d\n", abs(num));
+//	return 0;
+//}
+
+
+//fabs
+//求浮点数的绝对值
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	float num = -12.3f;
+//	printf("%.1f\n", fabs(num));
+//	return 0;
+//}
+
+
+//acos --- 反余弦
+//#include <stdio.h>     
+//#include <math.h>       
+//int main()
+//{
+//	double param, result;
+//	param = 0.5;
+//	result = acos(param) * 180.0 / pi;
+//	printf("The arc cosine of %f is %f degrees.\n", param, result);
+//	return 0;
+//}
+
+
+//求圆周率
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double pi = acos(-1.0);
+//	printf("%f\n", pi);
+//	return 0;
+//}
+
+
+//cosh
+//双曲余弦函数
